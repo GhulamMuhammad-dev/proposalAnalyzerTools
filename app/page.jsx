c3339db5053a35984dd71f6f70b51016110f7437
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2025-08-23 13:05:51 +0500

    make some changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,3 @@
-
-import CustomGrid from "@/components/Practice/CustomGrid";
-import Grid from "@/components/Practice/Grid";
-import GridEditor from "@/components/Practice/GridEditor";
 import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
